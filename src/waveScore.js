90e99375c974bd47e36144a3f11cb66a0b71f274
Scott Bergler <commandinghands@gmail.com>
2019-03-22 16:34:07 -0700

    confirm getSwellPeriod passes.

--- src/waveScore.js
+++ src/waveScore.js
@@ -6,7 +6,15 @@ class WaveScore {
   }
 
   getSwellPeriodScore() {
-    return 3;
+    if (this.swellPeriod >= 16) {
+      return 5;
+    } else if (this.swellPeriod >= 12) {
+      return 4;
+    } else if (this.swellPeriod >= 10) {
+      return 3;
+    } else {
+      return 1;
+    }
   }
 
 }
