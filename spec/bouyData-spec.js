8d91c19134acea590b2c1bcf331b8e6b3bec8b34
Scott Bergler <commandinghands@gmail.com>
2019-03-22 16:54:06 -0700

    attempt api call test. Getting false positive.

--- spec/bouyData-spec.js
+++ spec/bouyData-spec.js
@@ -2,5 +2,15 @@ import { BouyData } from './../src/bouyData';
 const mockData = require('../src/bouyDataMock.txt');
 
 describe('BouyData', () => {
+  it('getBouyData() should return correct data', () => {
+    spyOn(BouyData.prototype, 'getBouyData').and.returnValue(Promise.resolve(mockData));
+
+    const url = 'https://www.ndbc.noaa.gov/data/realtime2/46029.spec';
+    const bouyData = new BouyData();
+    bouyData.getBouyData(url).then((result) => {
+      expect(bouyData.prototype.getBouyData(url)).toHaveBeenCalledWith(url);
+      expect(result).toEqual(mockData);
+    })
+  });
 
 });
--- src/bouyData.js
+++ src/bouyData.js
@@ -2,7 +2,14 @@ class BouyData {
   constructor() {
   }
 
-  
+  getBouyData(url) {
+    return fetch(url)
+    .then((response) => {
+      let textResponse = response.text();
+      return textResponse;
+    });
+  }
+
 }
 
 export { BouyData };
